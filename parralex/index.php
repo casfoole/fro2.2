d2269d656e0d4a44d23778f28e3501dd44339f97
Cas Foole <22937@ma-web.nl>
2018-02-08 09:41:49 +0100

    8-2-18

---
+++ parralex/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="parallax" style=" background-image: url('donald.jpg');"></div>
+    <div class="text">
+        <h1>Titel 1</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum felis ac lectus volutpat, ac tempor libero vulputate. Quisque rutrum, magna in convallis cursus, magna eros laoreet orci, eget sodales velit ipsum sed tellus. Praesent eu ligula in nulla euismod pretium et vel urna. Sed a lorem nulla. Nam mollis elit vel mauris blandit rhoncus. Pellentesque ut augue luctus, tincidunt ligula nec, fringilla tellus. Mauris luctus sem id mi aliquet, ac scelerisque libero egestas. Aliquam blandit mi leo, at volutpat ex malesuada faucibus. Nunc vehicula mattis nulla, sagittis sollicitudin purus egestas ut. Suspendisse pretium purus metus, vel ullamcorper ipsum accumsan ac. Duis sit amet urna eu odio malesuada tincidunt rutrum ac diam.</p>
+    </div>
+<div class="parallax" style=" background-image: url('meer.jpg');"></div>
+    <div class="text">
+        <h1>Titel 2</h1>
+        <p>Nam ullamcorper iaculis ullamcorper. Donec egestas nunc turpis, eget efficitur felis dignissim id. Proin varius risus non pretium aliquam. Donec ac magna ut urna lacinia elementum. Nulla in aliquam leo, sit amet sollicitudin augue. Quisque a velit ullamcorper, hendrerit dolor nec, commodo ex. Vestibulum quis congue dui, sed rutrum lorem. Cras velit odio, consequat pellentesque est nec, lobortis pulvinar sapien. Proin vel odio fermentum, elementum sem non, mattis sem. Fusce mi ante, sagittis sed cursus a, sodales a ex. Maecenas a ultricies lacus, sit amet varius enim. Nam malesuada a turpis a cursus.</p>
+    </div>
+<div class="parallax" style=" background-image: url('vogel.jpg');"></div>
+    <div class="text">
+        <h1>Titel 3</h1>
+        <p>Vestibulum varius eget metus varius ornare. Morbi rutrum libero pretium est posuere, vitae vestibulum nisi elementum. Vestibulum mollis nec sem vel viverra. Sed venenatis velit sit amet ante fermentum tincidunt. Morbi tincidunt ligula et cursus faucibus. Vivamus ac elementum ex. Nulla tristique velit non convallis faucibus. Phasellus mi libero, dignissim id felis sed, vulputate congue elit. Duis laoreet est a lobortis congue. Etiam dapibus semper dolor eget ultricies. Proin enim eros, dapibus non commodo at, aliquam vitae mi. Quisque ut hendrerit lacus. In pretium tortor id diam scelerisque tempus id sed mi. Donec id consectetur purus.</p>
+    </div>
+</body>
+</html>
